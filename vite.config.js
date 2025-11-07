5f3758b48a22d4121e9acf2c1e7f195aa83d764e
tanrenal <tanurenaldi01@gmail.com>
2025-11-07 16:52:00 +0700

    Add Railway custom build config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0", // biar bisa diakses dari luar (via tunnel)
+    port: 5173, // port default vite
+    hmr: {
+      host: "localhost", // ganti jadi 'localhost' agar HMR tidak salah alamat
+    },
+  },
   plugins: [
     laravel({
       input: ["resources/css/app.css", "resources/js/app.js"],
